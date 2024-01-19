d0ab453cf14c9655f6cc78fa4c771bba93040dbe
juanpinheirx <jroliveirapinheiro@gmail.com>
2024-01-19 13:49:58 -0300

    Refactored to private attributes with getters and setters

--- index.js
+++ index.js
@@ -2,15 +2,48 @@
 class Television {
     constructor(b, s, r, c, cT) {
         console.log(`Creating tv from brand: ${b}`);
-        this.brand = b;
-        this.size = s;
-        this.resolution = r;
-        this.connections = c;
-        this.connectedTo = cT;
+        this._brand = b;
+        this._size = s;
+        this._resolution = r;
+        this._connections = c;
+        this._connectedTo = cT;
     }
-    // methods
+    // getters and setters
+    getBrand() {
+        return this._brand;
+    }
+    ;
+    setBrand(name) {
+        return this._brand = name;
+    }
+    getSize() {
+        return this._size;
+    }
+    ;
+    setSize(size) {
+        return this._size = size;
+    }
+    getResolution() {
+        return this._resolution;
+    }
+    ;
+    setResolution(resol) {
+        return this._resolution = resol;
+    }
+    getConnections() {
+        return this._connections;
+    }
+    setConnections(conx) {
+        return this._connections = conx;
+    }
+    getConnectedTo() {
+        return this._connectedTo;
+    }
+    setConnectedTo(coTo) {
+        return this._connectedTo = coTo;
+    }
     turnOn() {
-        console.log(`This tv belongs to the brand ${this.brand}. Its size is ${this.size}. Its resolution is ${this.resolution}. The available connections are: ${this.connections}. And this television is connected to ${this.connectedTo}.`);
+        console.log(`This tv belongs to the brand ${this._brand}. Its size is ${this._size}. Its resolution is ${this._resolution}. The available connections are: ${this._connections}. And this television is connected to ${this._connectedTo}.`);
     }
 }
 const tv = new Television('Samsung', '40"', '4k', 'HDMI', 'Brasil');
--- index.ts
+++ index.ts
@@ -1,21 +1,51 @@
 class Television {
   // attributes
-  brand: string;
-  size: string;
-  resolution: string;
-  connections: string;
-  connectedTo: string;
+  private _brand: string;
+  private _size: string;
+  private _resolution: string;
+  private _connections: string;
+  private _connectedTo: string;
   constructor(b: string, s: string, r: string, c: string, cT: string) {
     console.log(`Creating tv from brand: ${b}`);
-    this.brand = b;
-    this.size = s;
-    this.resolution = r;
-    this.connections = c;
-    this.connectedTo = cT;
+    this._brand = b;
+    this._size = s;
+    this._resolution = r;
+    this._connections = c;
+    this._connectedTo = cT;
   }
-  // methods
+  // getters and setters
+  getBrand() {
+    return this._brand;
+  };
+  setBrand(name: string) {
+    return this._brand = name;
+  }
+  getSize() {
+    return this._size;
+  };
+  setSize(size: string) {
+    return this._size = size;
+  }
+  getResolution() {
+    return this._resolution;
+  };
+  setResolution(resol: string) {
+    return this._resolution = resol;
+  }
+  getConnections() {
+    return this._connections;
+  }
+  setConnections(conx: string) {
+    return this._connections = conx;
+  }
+  getConnectedTo() {
+    return this._connectedTo;
+  }
+  setConnectedTo(coTo: string) {
+    return this._connectedTo = coTo;
+  }
   turnOn() {
-    console.log(`This tv belongs to the brand ${this.brand}. Its size is ${this.size}. Its resolution is ${this.resolution}. The available connections are: ${this.connections}. And this television is connected to ${this.connectedTo}.`);
+    console.log(`This tv belongs to the brand ${this._brand}. Its size is ${this._size}. Its resolution is ${this._resolution}. The available connections are: ${this._connections}. And this television is connected to ${this._connectedTo}.`);
   }
 }
 
